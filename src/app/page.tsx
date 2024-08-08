50b637a194abcbbc283771957b28f8d43c6e6a9b
yositasatiman <yosita_satiman@cmu.ac.th>
2024-08-08 15:29:36 +0700

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,8 +94,31 @@ export default function Home() {
           </div>
         </div>
 
+        <div className="d-flex gap-2 my-2">
+          <img
+            src="/profileImages/lisa.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Lisa
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>999 คน</span>
+            </div>
+          </div>
+        </div>
+
         {/* map-loop render Comment component here */}
-        {users && !isLoading && comments.map((user: UserCardProps) => (<UserCard key={user.email} {...user}/>))}
       </div>
     </div>
   );
